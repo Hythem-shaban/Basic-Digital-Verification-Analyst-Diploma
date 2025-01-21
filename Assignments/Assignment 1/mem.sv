5acd01628e9daa750bd2627122a24e93b6a6ae28
Hythem Ahmed <89210658+Hythem-shaban@users.noreply.github.com>
2025-01-21 08:25:20 +0200

    add assignment 1
    designing a single port memory and the verification plan

---
+++ Assignments/Assignment 1/mem.sv
@@ -0,0 +1,33 @@
+module memory 
+#(
+  parameter WIDTH = 32,
+  parameter DEPTH = 16
+) 
+(
+  input  logic                     i_clk,
+  input  logic                     i_rst_n,
+  input  logic [WIDTH-1:0]         i_data,
+  input  logic                     i_we,
+  input  logic [$clog2(DEPTH)-1:0] i_addr,
+  output logic [WIDTH-1:0]         o_data,
+  output logic                     o_valid
+);
+
+logic [WIDTH-1:0] mem [DEPTH];
+
+always @(posedge i_clk, negedge i_rst_n) begin
+  if (!i_rst_n) begin
+    for (int i = 0; i < DEPTH; i++) begin
+      mem[i] <= 0;
+    end
+  end
+  else if (i_we) begin
+    mem[i_addr] <= i_data;
+  end
+  else begin
+    o_data <= mem[i_addr];
+    o_valid <= 1'b1;
+  end
+end
+  
+endmodule
